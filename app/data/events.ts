7036114db069382d2b60e34e4d3c6159afc4687d
CalebLovell <caleblovell1@gmail.com>
2023-06-11 21:02:23 -0500

    add chatgpt event ideas. WIP

--- app/data/events.ts
+++ app/data/events.ts
@@ -3,70 +3,114 @@ export const events = [
 		title: `Today`,
 		date: new Date(),
 		country: `Paraguay`,
-		description:
-			`Latin America is dominated by the left-wing, including even Colombia for the first time in its history. However, the recent landslide election of Santiago Peña of Paraguay, and a telling vote against a new constitution in Chile may signal a change in the political winds yet again.`,
+		description: `Latin America is dominated by the left-wing, including even Colombia for the first time in its history. However, the recent landslide election of Santiago Peña of Paraguay, and a telling vote against a new constitution in Chile may signal a change in the political winds yet again.`,
 	},
 	{
 		title: `Second Pink Tide`,
 		date: new Date(2018, 0, 1),
 		country: `Mexico`,
-		description:
-			`AMLO is elected president of Mexico. The second pink tide begins, which will return nearly all of Latin America to left-wing rule over the next few years.`,
+		description: `AMLO is elected president of Mexico. The second pink tide begins, which will return nearly all of Latin America to left-wing rule over the next few years.`,
 	},
 	{
 		title: `Swing to the Right`,
 		date: new Date(2010, 2, 12),
 		country: `Chile`,
-		description:
-			`Piñera is elected president of Chile. He is the first of a wave of right-wing leaders to be elected in Latin America, ending the first pink tide. Macri, Bolsonaro, Trump and other right-wing leaders will balance out the previous decade of left-wing leaders.`,
+		description: `Piñera is elected president of Chile. He is the first of a wave of right-wing leaders to be elected in Latin America, ending the first pink tide. Macri, Bolsonaro, Trump and other right-wing leaders will balance out the previous decade of left-wing leaders.`,
 	},
 	{
 		title: `First Pink Tide`,
 		date: new Date(1999, 1, 2),
 		country: `Venezuela`,
-		description:
-			`Hugo Chávez is elected president of Venezuela. He is the first of a wave of left-wing leaders to be elected in Latin America. The next decade will see every country in South America elect a "pink" goverment; except for Colombia.`,
+		description: `Hugo Chávez is elected president of Venezuela. He is the first of a wave of left-wing leaders to be elected in Latin America. The next decade will see every country in South America elect a "pink" goverment; except for Colombia.`,
 	},
 	{
 		title: `Democratic Wave`,
 		date: new Date(1979),
 		country: `Ecuador`,
-		description:
-			`Aguilera is elected president of Ecuador, marking a return to democracy in Latin America. Over the next fifteen years, twenty countries in Latin America will return to democracy after decades of military rule.`,
+		description: `Aguilera is elected president of Ecuador, marking a return to democracy in Latin America. Over the next fifteen years, twenty countries in Latin America will return to democracy after decades of military rule.`,
 	},
 	{
 		title: `Operation Condor`,
 		date: new Date(1973, 10, 11),
 		country: `Chile`,
-		description:
-			`Pinochet seizes power in Chile. The most famous of the US-backed coups in Latin America, Operation Condor is a US-backed campaign to overthrow left-wing governments in Latin America during the height of the Cold War.`,
+		description: `Pinochet seizes power in Chile. The most famous of the US-backed coups in Latin America, Operation Condor is a US-backed campaign to overthrow left-wing governments in Latin America during the height of the Cold War.`,
+	},
+	{
+		title: `Cuban Revolution`,
+		date: new Date(1959, 0, 1),
+		country: `Cuba`,
+		description: `Fidel Castro leads a successful revolution in Cuba, overthrowing the US-backed Batista regime. The Cuban Revolution will inspire left-wing movements throughout Latin America, and will lead to the US-backed Bay of Pigs invasion in 1961.`,
+	},
+	{
+		title: `Mexican Revolution`,
+		date: new Date(1910, 10, 20),
+		country: `Mexico`,
+		description: `The Mexican Revolution begins, and will last until 1920. The revolution will lead to the creation of the Institutional Revolutionary Party (PRI), which will dominate Mexican politics for the next 70 years.`,
+	},
+	{
+		title: `Spanish-American War`,
+		date: new Date(1898, 3, 25),
+		country: `Cuba`,
+		description: `The Spanish-American War begins, and will last until August 12, 1898. The war will lead to the independence of Cuba, and the US will gain control of Puerto Rico, Guam, and the Philippines.`,
+	},
+	{
+		title: `The War of the Pacific`,
+		date: new Date(1879, 3, 5),
+		country: `Peru`,
+		description: `The War of the Pacific begins, and will last until 1884. The war will lead to the independence of Chile, and the loss of territory for Bolivia and Peru. The war will also lead to the rise of Chile as a regional power in South America.`,
+	},
+	{
+		title: `The Paraguayan War`,
+		date: new Date(1864, 3, 1),
+		country: `Paraguay`,
+		description: `The Paraguayan War begins, and will last until 1870. The war will lead to the independence of Paraguay, and the loss of territory for Argentina and Brazil. The war will also lead to the rise of Brazil as a regional power in South America.`,
+	},
+	{
+		title: `The Mexican-American War`,
+		date: new Date(1846, 3, 25),
+		country: `Mexico`,
+		description: `The Mexican-American War begins, and will last until February 2, 1848. The war will lead to the independence of Mexico, and the loss of territory for Mexico. The war will also lead to the rise of the United States as a regional power in North America.`,
+	},
+	{
+		title: `The Monroe Doctrine`,
+		date: new Date(1823, 11, 2),
+		country: `United States of America`,
+		description: `The Monroe Doctrine is issued by the United States, and will last until 2013. The doctrine will lead to the independence of the United States, and the loss of territory for the United States. The doctrine will also lead to the rise of the United States as a regional power in North America.`,
+	},
+	{
+		title: `The Battle of Ayacucho`,
+		date: new Date(1824, 11, 9),
+		country: `Peru`,
+		description: `The Battle of Ayacucho is fought, and will last until 1824. The battle will lead to the independence of Peru, and the loss of territory for Peru. The battle will also lead to the rise of Peru as a regional power in South America.`,
+	},
+	{
+		title: `The Battle of Carabobo`,
+		date: new Date(1821, 5, 24),
+		country: `Venezuela`,
+		description: `The Battle of Carabobo is fought, and will last until 1821. The battle will lead to the independence of Venezuela, and the loss of territory for Venezuela. The battle will also lead to the rise of Venezuela as a regional power in South America.`,
 	},
 	{
 		title: `Federal Republic of Central America`,
 		date: new Date(1821, 8, 15),
 		country: `Guatemala`,
-		description:
-			`The Federal Republic of Central America is formed by five Central American states after they declare independence from Spain, with their capital being Guatemala City. The union will last until 1838.`,
+		description: `The Federal Republic of Central America is formed by five Central American states after they declare independence from Spain, with their capital being Guatemala City. The union will last until 1838.`,
 	},
 	{
 		title: `Colombian Declaration of Independence`,
 		date: new Date(1810, 6, 20),
 		country: `Colombia`,
-		description:
-			`Colombia declares independence from Spain. Though the fight for independence will last another decade, Colombia begins the process of freeing itself from Spanish rule.`,
+		description: `Colombia declares independence from Spain. Though the fight for independence will last another decade, Colombia begins the process of freeing itself from Spanish rule.`,
 	},
 	{
 		title: `The Haitian Revolution`,
 		date: new Date(1791, 0, 1),
 		country: `Haiti`,
-		description:
-			`Toussaint Louverture leads a successful slave revolt in Haiti, leading to the creation of the first black republic in the world, and thus the beginning of post-colonial history in Latin America.`,
+		description: `Toussaint Louverture leads a successful slave revolt in Haiti, leading to the creation of the first black republic in the world, and thus the beginning of post-colonial history in Latin America.`,
 	},
 	{
 		title: `The American Revolution`,
 		date: new Date(1789, 3, 30),
 		country: `United States of America`,
-		description:
-			`After a hard fought war for independence from Great Britain, George Washington is elected the first President of the United States. The American Revolution will inspire independence movements throughout Latin America.`,
+		description: `After a hard fought war for independence from Great Britain, George Washington is elected the first President of the United States. The American Revolution will inspire independence movements throughout Latin America.`,
 	},
 ];
