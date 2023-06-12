89bdb1ae339cf9909d5ad6a6fcbeb646a6dd1515
CalebLovell <caleblovell1@gmail.com>
2023-06-12 00:57:10 -0500

    update events

--- app/data/events.ts
+++ app/data/events.ts
@@ -1,12 +1,12 @@
 export const events = [
 	{
-		title: `The American Revolution`,
+		title: `American Revolution`,
 		date: new Date(1789, 3, 30),
 		country: `United States of America`,
 		description: `After a hard fought war for independence from Great Britain, George Washington is elected the first President of the United States. The American Revolution will inspire independence movements throughout Latin America.`,
 	},
 	{
-		title: `The Haitian Revolution`,
+		title: `Haitian Revolution`,
 		date: new Date(1791, 0, 1),
 		country: `Haiti`,
 		description: `Toussaint Louverture leads a successful slave revolt in Haiti, marking the creation of the first black republic in the world, and thus the beginning of post-colonial history in Latin America.`,
@@ -24,7 +24,7 @@ export const events = [
 		description: `Colombia declares independence from Spain. Through the leadership of figures like Simón Bolívar, Colombia will fight Spain for the next decade. The eventual success of the revolution will eventually lead to the establishment of an independent state known as Gran Colombia, which will include modern-day Colombia, Venezuela, Ecuador, and Panama.`,
 	},
 	{
-		title: `The Mexican War of Independence`,
+		title: `Mexican War of Independence`,
 		date: new Date(1810, 9, 16),
 		country: `Mexico`,
 		description: `The Mexican War of Independence was a series of local and regional struggles that took place from 1810 to 1821, resulting in Mexico gaining independence from Spain. Led by figures like Miguel Hidalgo, José María Morelos, and Agustín de Iturbide, the conflict culminated in the collapse of royal government and the establishment of independent Mexico under a federal republic.`,
@@ -36,25 +36,25 @@ export const events = [
 		description: `The Federal Republic of Central America is formed by five Central American states after they declare independence from Spain, with their capital being Guatemala City. The union will last until 1838.`,
 	},
 	{
-		title: `The Battle of Ayacucho`,
+		title: `Battle of Ayacucho`,
 		date: new Date(1824, 11, 9),
 		country: `Peru`,
 		description: `The Battle of Ayacucho was a crucial military clash in the Peruvian War of Independence led by Simón Bolivar's lieutenant Antonio José de Sucre. A decisive victory by the Patriot forces would result in the independence of Peru and Bolivia, and the end of Spanish rule in South America.`,
 	},
 	{
-		title: `The Mexican-American War`,
+		title: `Mexican-American War`,
 		date: new Date(1846, 3, 25),
 		country: `Mexico`,
 		description: `The Mexican-American War was fought from 1846 to 1848, triggered by the American annexation of Texas. The United States invaded Mexico, and eventually captured Mexico City, leading to Mexico ceding a significant portion of its territory to the United States.`,
 	},
 	{
-		title: `The Paraguayan War`,
+		title: `Paraguayan War`,
 		date: new Date(1864, 3, 1),
 		country: `Paraguay`,
 		description: `The Paraguayan War, also called the War of the Triple Alliance, took place between 1864 and 1870, and was fought between Paraguay and the Triple Alliance composed of Argentina, Brazil, and Uruguay. It was the deadliest conflict in Latin American history, resulting in significant casualties and the loss of territory by Paraguay. Following Paraguay's defeat in conventional warfare, the conflict devolved into total guerilla war, as recalcitrant President Francisco Solano López refused to surrender until his death in 1870. The death toll was immense, with some estimates placing the losses of Paraguayan men at as high as 90% of the total population.`,
 	},
 	{
-		title: `The War of the Pacific`,
+		title: `War of the Pacific`,
 		date: new Date(1879, 3, 5),
 		country: `Chile`,
 		description: `The War of the Pacific was a conflict between Chile and a Bolivian-Peruvian alliance from 1879 to 1884, sparked by a dispute over nitrate taxation. Chile emerged victorious, gaining territory from Peru and Bolivia, and leaving Bolivia permanently landlocked.`,
