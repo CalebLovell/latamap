e262c4bc40de28b1e308f54e20db17ee0dc6d727
CalebLovell <caleblovell1@gmail.com>
2023-06-16 21:57:48 -0500

    shrink key on mobile

--- app/components/Key.tsx
+++ app/components/Key.tsx
@@ -17,8 +17,8 @@ export const Key = () => {
 				{labels.map((label, index) => (
 					<div key={label} className='mt-1 flex items-center'>
 						{/* @ts-ignore */}
-						<div className='mr-2 h-10 w-1.5 transition duration-500 ease-in-out' style={{ backgroundColor: colors[index + 1] }} />
-						<p className='text-sm font-semibold text-black'>{label}</p>
+						<div className='mr-2 h-6 w-1.5 transition duration-500 ease-in-out sm:h-10' style={{ backgroundColor: colors[index + 1] }} />
+						<p className='text-xs font-semibold text-black sm:text-sm'>{label}</p>
 					</div>
 				))}
 			</div>
