e90076850f59fc9cd9e75a1723a8df63257e38f2
CalebLovell <caleblovell1@gmail.com>
2024-01-19 01:20:11 -0600

    run clean script

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner = () => {
 	const { bannerIsOpen, setBannerIsOpen } = useAppStore();
 
 	return (
-		<div className='absolute w-full px-2 max-w-3xl'>
+		<div className='absolute w-full max-w-3xl px-2'>
 			<div className='relative w-full'>
 				<div
 					className='absolute z-10 mt-14 flex w-full items-center gap-x-6 rounded-md bg-blue-200 px-4 py-2.5 sm:px-3.5 sm:before:flex-1'
@@ -14,8 +14,8 @@ export const Banner = () => {
 				>
 					<p className='text-sm leading-6'>
 						Welcome Twitter! I will be updating the data to fix mistakes throughout the day. Thanks for your patience! In the meantime, please
-						consider reading the disclaimer in the side menu. I am aware that left-right labels are reductive, and MUCH less meaningful before the
-						20th century. That is a limitation of the project. Don&apos;t take it too seriously!
+						consider reading the disclaimer in the side menu. I am aware that left-right labels are reductive, and MUCH less meaningful before
+						the 20th century. That is a limitation of the project. Don&apos;t take it too seriously!
 					</p>
 					<div className='flex flex-1 justify-end'>
 						<button type='button' onClick={() => setBannerIsOpen(false)} className='-m-3 p-3 focus-visible:outline-offset-[-4px]'>
